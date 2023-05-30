33d3b11c01d4ef4299a26746086db77dde476559
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-05-29 17:36:10 -0700

    minor update

---
+++ fp.js
@@ -0,0 +1,7 @@
+new fullpage('#fullpage', {
+    licenseKey: '4HLKJ-HJ0M9-3K85I-ADMJK-OJXXM',
+    navigation: true,
+    navigationPosition: 'left',
+    navigationTooltips: ['Home', 'Bio', 'Projects', 'Class Assignments'],
+    showActiveTooltip: true
+  });
